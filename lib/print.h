f712828349b73938dee608ec4798d4e519205e8b
Daniele Cioffi <danielecioffi@MBP-di-Daniele.home-life.hub>
2021-05-28 12:14:27 +0200

    print.h modified

--- lib/print.h
+++ lib/print.h
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#define MYFILE "test.csv"
+#include <string.h>
+#define MYFILE "prova.csv"
 
-void print_file(char *name, double time, int size, double energy, int nThreads) {
+void print_file(char *name_csv, char *name, double time, int size, double energy, int nThreads)
+{
     FILE *f;
     int s;
 
     //File opening .csv
-    if ((f = fopen(MYFILE, "a+")) == NULL) {
+    if ((f = fopen(name_csv, "a+")) == NULL)
+    {
         printf("Errore opening file...\n");
     }
 
@@ -15,12 +18,22 @@ void print_file(char *name, double time, int size, double energy, int nThreads) 
     s = ftell(f);
 
     //If it is empty it is initialized with the label names
-    if (s == 0) {
+    if (s == 0)
+    {
         printf("Empty File... Initialization!\n");
-        fprintf(f, "%s, %s, %s, %s, %s\n", "NAMEFILE", "TIME", "SIZE", "ENERGY", "NTHREAD");
+        fprintf(f, "%s| %s| %s| %s| %s\n", "NAMEFILE", "TIME", "SIZE", "ENERGY", "NTHREAD");
     }
 
+    /* //Saving values ​​in csv file
+    fprintf(f, "%s, %f, %d, %f, %d\n", name, time, size, energy, nThreads); */
+
+    int time_1 = (int)time;
+    int time_2 = (time - time_1) * 1000;
+
+    int energy_1 = (int)energy;
+    int energy_2 = (energy - energy_1) * 1000;
     //Saving values ​​in csv file
-    fprintf(f, "%s, %f, %d, %f, %d\n", name, time, size, energy, nThreads);
+    fprintf(f, "%s| %d,%d| %d| %d,%d| %d\n", name, time_1, time_2, size, energy_1, energy_2, nThreads);
+
     fclose(f);
 }
